5c6d0625529390947f72d569d7756c72bff0e616
bijay <bijay.mandal@primotech.com>
2023-08-28 20:02:59 +0530

    End point change save_redemption

--- lib/utils/API_Constant.dart
+++ lib/utils/API_Constant.dart
@@ -13,7 +13,7 @@ abstract class BaseUrl {
 const String kAppVersion = "2.0.1";
 
 class Api {
-  static const String baseUrl = BaseUrl.stageUrl;
+  static const String baseUrl = BaseUrl.live;
   static const String baseImageUrl = BaseUrl.liveImage;
   static const String baseTangoCardUrl = BaseUrl.liveTangoCard;
 
--- lib/views/home/Home.dart
+++ lib/views/home/Home.dart
@@ -124,21 +124,23 @@ class _HomeState extends State<Home> {
       setState(() {
         if (sharedPreferences.getString("is_first") == "0") {
           if (pointInt < 100) {
-            int remainPoint = 100 - pointInt;
-            setState(() {
-              pointCheckDialog(context,
-                  "You are just ${remainPoint.toString()} points away.");
-            });
+            // int remainPoint = 100 - pointInt;
+            // setState(() {
+            //   pointCheckDialog(context,
+            //       "You are just ${remainPoint.toString()} points away.");
+            // });
+            dialogAlert(context, Message().RedeemConfirmMsg);
           } else {
             dialogAlert(context, Message().RedeemConfirmMsg);
           }
         } else {
           if (pointInt < 50) {
-            int remainPoint = 50 - pointInt;
-            setState(() {
-              pointCheckDialog(context,
-                  "You are just ${remainPoint.toString()} points away.");
-            });
+            // int remainPoint = 50 - pointInt;
+            // setState(() {
+            //   pointCheckDialog(context,
+            //       "You are just ${remainPoint.toString()} points away.");
+            // });
+            dialogAlert(context, Message().RedeemConfirmMsg);
           } else {
             dialogAlert(context, Message().RedeemConfirmMsg);
           }
